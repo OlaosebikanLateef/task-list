a885433d0e220b59f207f67504d854aa2dc5fd5c
OlaosebikanLateef <laolaosebikan@gmail.com>
2024-09-16 03:01:12 +0400

    new comment

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,31 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Training</title>
-</head>
-<body>
-    <h1>
-        The list of tasks
-      </h1>
+@extends('layouts.app')
+@section('title', 'The list of tasks')
+
+@section('content')
       
       <div>
         @if (count($tasks))
             @foreach ($tasks as $tasks)
-                <div>{{$tasks->title}}</div>
+                <div><a href="{{ route('tasks.show', ['id' => $tasks->id]) }}">{{ $tasks->title }}</a></div>
             @endforeach    
         @else
           <div>There are no tasks!</div>     
         @endif
         {{-- @endforelse --}}
       </div>
+      @endsection
    
-</body>
-</html>
+
 
    
-{{-- <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a> --}}
-
 
